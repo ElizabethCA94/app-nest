eed440befc53df4fac50546cc4bd06a89c33658d
ElizabethCA94 <lizsorbe94@hotmail.com>
2019-04-14 00:11:37 -0500

    conect database 'appnest_db'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiculosController } from './vehiculos/vehiculos.controller';
 import { ClientesController } from './clientes/clientes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'nest',
+        password: 'app',
+        database: 'appnest_db',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController, VehiculosController, ClientesController],
   providers: [AppService],
 })
